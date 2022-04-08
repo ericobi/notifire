ef5d38d3c871d9dc0be750deef20ca696e1569ce
ninjadev101 <sanchzxel@gmail.com>
2022-04-08 00:25:11 -0300

    ✅  added matrix tests

--- providers/matrix/src/lib/matrix.provider.spec.ts
+++ providers/matrix/src/lib/matrix.provider.spec.ts
@@ -1,3 +1,25 @@
 import { MatrixSmsProvider } from './matrix.provider';
 
-test('should trigger matrix library correctly', async () => {});
+test('should trigger Twilio correctly', async () => {
+  const provider = new MatrixSmsProvider({ opts: 'https://matrix.org' });
+  const spy = jest
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    .spyOn(provider.matrixClient.on, 'Room.timeline');
+
+  await provider.sendMessage({
+    to: 'matrix:@example.com',
+    content: 'SMS Content',
+  });
+
+  expect(spy).toHaveBeenCalled();
+  expect(spy).toHaveBeenCalledWith(
+    {
+      eventType: 'm.room.message',
+      content: {
+        body: 'SMS Content',
+      },
+    },
+    'matrix:@example.com'
+  );
+});
